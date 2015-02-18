f86559584ae335b9323db752d8476b713cb17b18
Jordan Collier <jordanicollier@gmail.com>
2015-02-18 09:46:46 -0700

    answer problem

--- solver.rb
+++ solver.rb
@@ -21,8 +21,15 @@ examples = [
   { input: "5531", output: 0 }
 ]
 
+
 class Solver
-  def self.solve(input)
-    0
-  end
+ def self.solve(input)
+   num = 0
+   hash = {"0" => 1, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 1, "7" => 0, "8" => 2, "9" => 1}
+   arr = input.split('')
+   arr.each do |x|
+     num = num + hash[x]
+   end
+   return num
+ end
 end
